c766aa53325d304c0ceaa0021e2844fa4e930fea
Avi Tzurel <avi@kensodev.com>
2012-09-20 13:21:08 +0300

    Adding the dummy class and spec_helper

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '0acae80212a0a5d9ab0e66e918595b9390d09aeb1977982edd101265478e8e76f46b223365a0530c6aad65d7a259618a2420dfbdabb1072b453fed656348119d'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+ENV["RAILS_ENV"] ||= 'test'
+
+require 'rubygems'
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require File.expand_path("../../spec/dummy/config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+end
